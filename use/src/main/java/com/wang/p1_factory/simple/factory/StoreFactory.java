93e38359ca20ff29b949dfa01a14761ac507058f
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-05-26 15:22:47 +0800

    工厂模式

---
+++ use/src/main/java/com/wang/p1_factory/simple/factory/StoreFactory.java
@@ -0,0 +1,11 @@
+package com.wang.p1_factory.simple.factory;
+
+public class StoreFactory {
+    public ICommodity getCommodityService(Integer commodityType) {
+        if (null == commodityType) return null;
+        if (1 == commodityType) return new CouponCommodityService();
+        if (2 == commodityType) return new GoodsCommodityService();
+        if (3 == commodityType) return new CardCommodityService();
+        throw new RuntimeException("不存在的商品服务类型");
+    }
+}
--- use/src/test/java/com/wang/ApiTest.java
+++ use/src/test/java/com/wang/ApiTest.java
@@ -1,19 +1,23 @@
 package com.wang;
 
 import com.alibaba.fastjson.JSON;
+import com.wang.p1_factory.simple.factory.ICommodity;
+import com.wang.p1_factory.simple.factory.StoreFactory;
 import com.wang.p1_factory.simple.old.work.AwardReq;
 import com.wang.p1_factory.simple.old.work.AwardRes;
 import com.wang.p1_factory.simple.old.work.PrizeController;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class Test {
+public class ApiTest {
 
-    private Logger logger = LoggerFactory.getLogger(Test.class);
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
 
-    @org.junit.Test
+    @Test
     public void test_awardToUser() {
 
         PrizeController prizeController = new PrizeController();
@@ -56,6 +60,33 @@ public class Test {
         AwardRes awardRes03 = prizeController.awardToUser(req03);
         logger.info("请求参数：{}", JSON.toJSON(req03));
         logger.info("测试结果：{}", JSON.toJSON(awardRes03));
+
+    }
+
+    @Test
+    public void test_commodity() throws Exception {
+        StoreFactory storeFactory = new StoreFactory();
+
+        // 1. 优惠券
+        ICommodity commodityService_1 = storeFactory.getCommodityService(1);
+        commodityService_1.sendCommodity("10001", "EGM1023938910232121323432", "791098764902132", null);
+
+        // 2. 实物商品
+        ICommodity commodityService_2 = storeFactory.getCommodityService(2);
+        Map<String, String> extMap = new HashMap<String, String>();
+        extMap.put("consigneeUserName", "谢飞机");
+        extMap.put("consigneeUserPhone", "15200292123");
+        extMap.put("consigneeUserAddress", "吉林省.长春市.双阳区.XX街道.檀溪苑小区.#18-2109");
+
+        commodityService_2.sendCommodity("10001", "9820198721311", "1023000020112221113", new HashMap<String, String>() {{
+            put("consigneeUserName", "谢飞机");
+            put("consigneeUserPhone", "15200292123");
+            put("consigneeUserAddress", "吉林省.长春市.双阳区.XX街道.檀溪苑小区.#18-2109");
+        }});
+
+        // 3. 第三方兑换卡(爱奇艺)
+        ICommodity commodityService_3 = storeFactory.getCommodityService(3);
+        commodityService_3.sendCommodity("10001", "AQY1xjkUodl8LO975GdfrYUio", null, null);
 
     }
 }
