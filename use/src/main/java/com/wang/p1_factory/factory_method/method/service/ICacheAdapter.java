da5d4700f561d9a2862597dbba5c522fe4021a69
wsz <94780184@qq.com>
2023-05-26 17:22:48 +0800

    feat: 动态代理适配工厂方法

---
+++ use/src/main/java/com/wang/p1_factory/factory_method/method/service/ICacheAdapter.java
@@ -0,0 +1,15 @@
+package com.wang.p1_factory.factory_method.method.service;
+
+import java.util.concurrent.TimeUnit;
+
+public interface ICacheAdapter {
+
+    String get(String key);
+
+    void set(String key, String value);
+
+    void set(String key, String value, long timeout, TimeUnit timeUnit);
+
+    void del(String key);
+
+}
---
+++ use/src/main/java/com/wang/p1_factory/factory_method/method/service/impl/IIRCacheAdapter.java
@@ -0,0 +1,29 @@
+package com.wang.p1_factory.factory_method.method.service.impl;
+
+
+import com.wang.p1_factory.factory_method.method.service.ICacheAdapter;
+import com.wang.p1_factory.factory_method.old.matter.IIR;
+
+import java.util.concurrent.TimeUnit;
+
+public class IIRCacheAdapter implements ICacheAdapter {
+
+    private IIR iir = new IIR();
+
+    public String get(String key) {
+        return iir.get(key);
+    }
+
+    public void set(String key, String value) {
+        iir.set(key, value);
+    }
+
+    public void set(String key, String value, long timeout, TimeUnit timeUnit) {
+        iir.setExpire(key, value, timeout, timeUnit);
+    }
+
+    public void del(String key) {
+        iir.del(key);
+    }
+
+}
